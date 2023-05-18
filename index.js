d534885f177dd0b38971d317890f98a6b2ff7784
yo <esteban.altamura@gmail.com>
2023-05-18 10:36:58 -0300

    actualizacion

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ getUpdatedData.getUpdatedData()
 
 
 const server = http.createServer(async (req, res) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
